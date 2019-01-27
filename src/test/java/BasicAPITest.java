8f56273da6b12353e1b07145a37c38629ee41dd8
tazhang <taozhangutd@gmail.com>
2019-01-27 10:12:21 -0600

    initial version

---
+++ src/test/java/BasicAPITest.java
@@ -0,0 +1,27 @@
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import io.restassured.RestAssured;
+import static org.hamcrest.Matchers.*;
+
+public class BasicAPITest {
+
+    @BeforeMethod
+    public void envSetUp() {
+        //Use faked APIs provided by https://jsonplaceholder.typicode.com
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    }
+
+    @Test
+    public void testStatusCode() {
+
+        //Basic Restful GET method, verify that statusCode is 200 OK
+        RestAssured.given().
+        when().
+                log().all().
+                get("/users/2/").
+        then().
+                log().all().
+                statusCode(200);
+
+    }
+}
